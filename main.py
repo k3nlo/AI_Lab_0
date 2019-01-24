a4b3915ef5c58d94761f4762b2fb01ce175a6c7b
kenlo <kenlo.dinhvan@gmail.com>
2019-01-24 17:10:39 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#print hello world
+
+# print = System.out.println
+print('Hello World')
+
+# 2 lists
+spanish = ['Hola','Mundo']
+french = ['Bonjour','le','Monde']
+
+for word in spanish: print(word)
+for word in french: print(word)
+
+import this
