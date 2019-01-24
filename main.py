82484b1b046454cb1b1ee37639670067baa337b3
kenlo <kenlo.dinhvan@gmail.com>
2019-01-24 17:29:48 -0500

    continued tutorial

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ for word in spanish: print(word)
 for word in french: print(word)
 
 import this
+
+a = 3
+b = [1,2,3]
+c = 4
+
+if a in b:
+    print(True)
+else : print(False)
+if c not in b: print(c, 'is not the list b')
